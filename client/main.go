da10bf972857486668d89eb7f6beac97aff5bd79
uk0 <zhangjianxinnet@gmail.com>
2026-04-03 02:43:33 +0800

    Ensure TUN cleanup on signal kill (SIGINT/SIGTERM) in GUI mode

--- client/main.go
+++ client/main.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	"gioui.org/app"
 
@@ -11,8 +13,21 @@ import (
 )
 
 func main() {
+	var a *ui.App
 	go func() {
-		a := ui.NewApp()
+		a = ui.NewApp()
+
+		// Catch OS signals for cleanup (kill, Ctrl+C, etc.)
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+		go func() {
+			<-sigCh
+			if a != nil && a.Client != nil {
+				a.Client.Disconnect()
+			}
+			os.Exit(0)
+		}()
+
 		if err := a.Run(); err != nil {
 			os.Exit(1)
 		}
